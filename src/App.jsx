0417fcf761a42fa206e6276d1fd771ff80d2dc76
ZI FANG <fangzi@ZIs-MacBook-Pro.local>
2023-12-14 21:28:01 -0500

    Added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
 import { useState } from "react";
-// import reactLogo from './assets/react.svg'
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   // console.log(puppies);
 
+  function handleClick() {
+    // to be added
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  // console.log(featuredPup);
+
   return (
-    <>
-      <div className="App">
-        {puppies.map((puppy) => {
-          return <p key={puppy.id}> {puppy.name} </p>;
-        })}
+    <div className="App">
+      <div className="Puplist">
+      <h2 style={{ color: 'Purple' }}> Puppy List </h2>
+      {puppies.map((puppy) => {
+        return (
+          <p className="PuppyName"
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
       </div>
-    </>
+      <div className="ThePup">
+        {featPupId && (
+          <div>
+            <h2 style={{ color: 'Purple' }}>{featuredPup.name}</h2>
+            <ul style={{ color: 'Purple' }}>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
